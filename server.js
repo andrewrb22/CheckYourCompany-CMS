5b34ed1696dacd3c53d081cf2ab10337313177eb
Andrew Rodriguez <andrewrb22@gmail.com>
2020-10-13 19:47:13 -0400

    created first prompt and switch to next prompt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const { start } = require("repl");
 
 // connection for the sql database
 var connection = mysql.createConnection({
@@ -21,5 +22,48 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-
+startprompt();
 });
+
+function startprompt() {
+inquirer.prompt([
+    {
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices:["Add","View","Update","Delete"]
+},
+{
+    name:"option",
+    type:"list",
+    message:"Select from the options below",
+    choices:["Employee","Role","Department"]
+    
+
+},
+
+// swicth to the next prompt base on the previews answers 
+
+]) .then(function (res) {
+    console.log(`You chose to ${res.action} a ${res.option}`);
+
+    switch (res.action) {
+        case "Add":
+            createData(res.option);
+            break;
+        case "View":
+            readData(res.option);
+            break;
+        case "Update":
+            updateData(res.option);
+            break;
+        case "Delete":
+            deleteData(res.option);
+            break;
+    }
+})
+.catch(function (err) {
+    console.log(err);
+})
+}
+    
